34238fd1964cf00af28ca4ee266640926763b558
hatice_v_daalen <HaticevanD@users.noreply.github.com>
2026-04-02 14:57:03 +0200

    feat: add validation schemas and middlewares for contacts

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -7,13 +7,28 @@ import {
   deleteContactById,
 } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import { isValidId } from '../middlewares/isValidId.js';
+import {
+  createContactSchema,
+  updateContactSchema,
+} from '../validation/contacts.js';
 
 const router = Router();
 
 router.get('/', ctrlWrapper(getContacts));
-router.get('/:contactId', ctrlWrapper(getContactById));
-router.post('/', ctrlWrapper(createNewContact));
-router.patch('/:contactId', ctrlWrapper(patchContact));
-router.delete('/:contactId', ctrlWrapper(deleteContactById));
+router.get('/:contactId', isValidId, ctrlWrapper(getContactById));
+router.post(
+  '/',
+  validateBody(createContactSchema),
+  ctrlWrapper(createNewContact),
+);
+router.patch(
+  '/:contactId',
+  isValidId,
+  validateBody(updateContactSchema),
+  ctrlWrapper(patchContact),
+);
+router.delete('/:contactId', isValidId, ctrlWrapper(deleteContactById));
 
 export default router;
