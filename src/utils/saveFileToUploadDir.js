4b1e0ff747753157c0384914b93a938b4b6cdd41
hatice_v_daalen <HaticevanD@users.noreply.github.com>
2026-04-11 13:49:49 +0200

    feat:lexical fixes performed

--- src/utils/saveFileToUploadDir.js
+++ src/utils/saveFileToUploadDir.js
@@ -4,17 +4,17 @@ import { TEMP_UPLOAD_DIR, UPLOAD_DIR } from '../constants/index.js';
 import { env } from './env.js';
 
 export const saveFileToUploadDir = async (file) => {
-  // 1. Dosyanın şu anki (geçici) yerini ve hedef (kalıcı) yerini belirliyoruz
+  // 1.Temporary and Final directory of the file
   const tempPath = path.join(TEMP_UPLOAD_DIR, file.filename);
   const finalPath = path.join(UPLOAD_DIR, file.filename);
 
-  // 2. Dosyayı temp klasöründen uploads klasörüne taşıyoruz
+  // 2. Carry the file from temp to uploads
   await fs.rename(tempPath, finalPath);
 
-  // 3. İstemcinin bu dosyaya tarayıcıdan erişebilmesi için URL oluşturuyoruz
-  // Örn: http://localhost:3000/uploads/resim-adi.jpg
+  // 3. Create URL so that the client can access to this file from browser
+  // Ex: http://localhost:3000/uploads/photo-name.jpg
   const PORT = env('PORT', '3000');
-  const APP_DOMAIN = env('APP_DOMAIN'); // Eğer domain tanımlıysa onu kullanır
+  const APP_DOMAIN = env('APP_DOMAIN'); // Use domain if defined
 
   return `${APP_DOMAIN || `http://localhost:${PORT}`}/uploads/${file.filename}`;
 };
